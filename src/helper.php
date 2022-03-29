3471ababecf63fd37adf42fa1ee8020a0ea6ba22
wayren <77871399@qq.com>
2022-03-29 11:25:32 +0800

    1

---
+++ src/helper.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @Author: Wayren
+ * @Date:   2022-03-29 10:47:48
+ * @Last Modified by:   Wayren
+ * @Last Modified time: 2022-03-29 11:25:17
+ */
+
+function loader($value='')
+{
+    // code...
+    // s
+    // ss
+    // a
+}
