dd8c7137f5a58b458e1df5b1a200088a33ac3b5e
Wayren <wayrenc@gmail.com>
2022-03-29 12:19:35 +0800

    1

--- src/Helper/Helper.php
+++ src/Helper/Helper.php
@@ -4,14 +4,19 @@ namespace lim\Helper;
  * @Author: Wayren
  * @Date:   2022-03-29 12:12:06
  * @Last Modified by:   Wayren
- * @Last Modified time: 2022-03-29 12:14:32
+ * @Last Modified time: 2022-03-29 12:16:22
  */
 
 
 class Helper 
 {
 	
-	function __construct(argument)
+	function __construct()
+	{
+		// code...
+	}
+
+	public function FunctionName($value='')
 	{
 		// code...
 	}
