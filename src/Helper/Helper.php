9004cd90973b2308cc041a8748df2a09e301c62c
Wayren <wayrenc@gmail.com>
2022-03-29 12:15:12 +0800

    1

---
+++ src/Helper/Helper.php
@@ -0,0 +1,18 @@
+<?php
+namespace lim\Helper;
+/**
+ * @Author: Wayren
+ * @Date:   2022-03-29 12:12:06
+ * @Last Modified by:   Wayren
+ * @Last Modified time: 2022-03-29 12:14:32
+ */
+
+
+class Helper 
+{
+	
+	function __construct(argument)
+	{
+		// code...
+	}
+}
--- src/helper/help.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @Author: Wayren
- * @Date:   2022-03-29 12:12:06
- * @Last Modified by:   Wayren
- * @Last Modified time: 2022-03-29 12:12:08
- */
