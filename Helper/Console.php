bdb3f46fe923b273c17a854b8f58bb320c8c6eaf
Wayren <77871399@qq.com>
2022-03-30 17:34:15 +0800

    1648632855

---
+++ Helper/Console.php
@@ -0,0 +1,115 @@
+<?php
+namespace lim\Helper;
+
+/**
+ * @Author: Wayren
+ * @Date:   2022-03-29 12:12:06
+ * @Last Modified by:   Wayren
+ * @Last Modified time: 2022-03-30 17:21:11
+ */
+
+use function Swoole\Coroutine\Http\post;
+use function Swoole\Coroutine\run;
+use Swoole\Coroutine\Http\Client\Exception;
+
+// use lim\Helper\Gateway;
+// use \swoole\Timer;
+
+class Console
+{
+    public function __construct($argv)
+    {
+        array_shift($argv);
+        if (!$method = array_shift($argv)) {
+            return;
+        }
+
+        echo $method;
+        $this->vars = $argv;
+
+        try {
+            $this->$method();
+        } catch (Throwable $e) {
+            print_r($e);
+        }
+
+    }
+
+    public function self()
+    {
+        $to   = dirname(__LIM__) . '/helper';
+        $sync = 'cp -r ' . __DIR__ . '/ ' . $to . ' && cd ' . $to . ' && sudo git add . && sudo git commit -m \'' . time() . '\' && sudo git push';
+        shell_exec($sync);
+    }
+
+    public function git()
+    {
+        $action = array_shift($this->vars);
+        switch ($action) {
+            case 'push':
+                $branch = array_shift($this->vars) ?? 'dev';
+                $msg    = array_shift($this->vars) ?? time();
+                $script = 'sudo git add . && sudo git commit -m \'' . $msg . '\' && sudo git push origin ' . $branch;
+                shell_exec($script);
+                break;
+            default:
+                // code...
+                break;
+        }
+    }
+
+    public function gateway()
+    {
+        $action = array_shift($this->vars);
+        $dir    = __LIM__ . '/app/Gateway';
+        switch ($action) {
+            case 'build':
+                if (!is_dir($dir)) {
+                    shell_exec('sudo mkdir ' . $dir);
+                }
+                break;
+            case 'start':
+             
+                $file = match (array_shift($this->vars)) {
+                    'hyperf' => __LIM__. '/config/gateway.php',
+                    default => __LIM__. '/config/gateway.php',
+                };
+      
+                if (!is_file($file)) {
+                    echo "配置文件不存在\n";
+                    return;
+                }
+                
+                $opt = include $file;
+                $daemonize = '-d'==array_shift($this->vars) ? true :false;
+                // file_put_contents(__LIM__.'/aa.json',json_encode($opt,JSON_PRETTY_PRINT));
+                $srv = new Gateway;
+                $srv->widthOption($opt)->run($daemonize);
+                break;
+            case 'reload':
+                $pid = file_get_contents('/var/log/' . str_replace('/', '_', __LIM__) . '.pid');
+                echo shell_exec('sudo kill -10 ' . $pid);
+                break;
+            case 'stop':
+                $pid = file_get_contents('/var/log/' . str_replace('/', '_', __LIM__) . '.pid');
+                echo shell_exec('sudo kill -15 ' . $pid);
+                break;
+            case 'register':
+                break;
+            default:
+                return;
+        }
+        echo ' -> ' . $action . PHP_EOL;
+    }
+
+    public function hf()
+    {
+        // require __LIM__ . '/bin/hyperf.php';
+        print_r($this);
+    }
+
+    public function __call($method, $argv)
+    {
+        // code...
+    }
+}
---
+++ Helper/Gateway.php
@@ -0,0 +1,242 @@
+<?php
+namespace lim\Helper;
+
+/**
+ * @Author: Wayren
+ * @Date:   2022-03-29 12:12:06
+ * @Last Modified by:   Wayren
+ * @Last Modified time: 2022-03-30 17:32:55
+ */
+
+use function Swoole\Coroutine\Http\post;
+use function Swoole\Coroutine\run;
+use Swoole\Coroutine\Http\Client\Exception;
+use \swoole\Timer;
+
+class Gateway
+{
+
+    public static $Gateway, $service = null;
+
+    public function widthOption($service = [])
+    {
+        // self::$service = $service;
+        $this->ip = (int) $service['gateway']['ip'] ?? null;
+        $this->port = (int) $service['gateway']['port'] ?? 9500;
+
+        foreach ($service['services'] ?? [] as $serviceType => $list) {
+
+            foreach ($list as $serviceName => $res) {
+                $this->service[$serviceName] = ['type' => $serviceType, 'url' => $res['url']];
+                // print_r([$serviceName,$serviceType,$res['list']]);
+                // print_r($res['list']);
+                foreach ($res['list'] as $api => $path) {
+                    $api = is_numeric($api) ? $path :$api;
+            
+                    if (isset($this->apiList[$api])) {
+                        echo "网关接口存在重复项:" . $api . PHP_EOL;
+                    }
+                    switch ($serviceType) {
+                        case 'http':
+                            $this->apiList[$api] = ['type' => $serviceType, 'url' => $res['url'] . $path];
+                            break;
+                        case 'rpc':
+                            $this->apiList[$api] = ['type' => $serviceType, 'name' => $serviceName, 'method' => $path, 'url' => $res['url']];
+                    }
+                }
+            }
+
+        }
+
+        // print_r($this->apiList);
+        return $this;
+    }
+
+    function run($daemonize = false)
+    {
+        if (!$this->ip) {
+            echo "配置文件不存在\n";
+            return;
+        }
+
+        \Swoole\Coroutine::set(['enable_deadlock_check' => null, 'hook_flags' => SWOOLE_HOOK_ALL]);
+
+        $config = [
+
+            'reactor_num'        => 1,
+            // 'worker_num'            => (int) WORKER_NUM,
+            // 'task_worker_num'       => (int) TASK_WORKER_NUM,
+            // 'task_enable_coroutine' => true,
+            'enable_coroutine'   => true,
+            'pid_file'           => '/var/log/' . str_replace('/', '_', __LIM__) . '.pid',
+            'log_level'          => SWOOLE_LOG_WARNING,
+            'hook_flags'         => SWOOLE_HOOK_ALL,
+            'max_wait_time'      => 1,
+            'reload_async'       => true,
+            'package_max_length' => 5 * 1024 * 1024,
+            // 'max_coroutine'         => (int) MAX_COROUTINE,
+            'daemonize'          => $daemonize,
+        ];
+
+        self::$Gateway = new \Swoole\WebSocket\Server($this->ip,$this->port);
+        self::$Gateway->set($config);
+        self::$Gateway->on('start', fn() => cli_set_process_title('Gateway'));
+        self::$Gateway->on('managerstart', [$this, 'managerstart']);
+        self::$Gateway->on('WorkerStart', [$this, 'WorkerStart']);
+        // self::$server->on('task', [$this, 'task']);
+        self::$Gateway->on('request', [$this, 'request']);
+        self::$Gateway->on('message', [$this, 'message']);
+        self::$Gateway->start();
+    }
+
+    function managerstart($server)
+    {
+        cli_set_process_title('-Manager');
+
+        // Timer::tick(1000 * 10, function () {
+        //     echo time().PHP_EOL;
+        //     // self::$server->task(['run' => $k]);
+        // });
+        echo ("服务启动成功\n");
+    }
+
+    function WorkerStart($server, int $workerId)
+    {
+
+        try {
+
+            // echo "服务启动\n";
+            Timer::clearAll();
+            if (function_exists('opcache_reset')) {
+                opcache_reset();
+            }
+
+            // wlog('缓存配置');
+
+            //同步配置文件
+            // Timer::tick(10 * 1000, fn() => static::$extend = uc('config'));
+
+            if ($server->taskworker) {
+                $id = $workerId - $server->setting['worker_num'];
+
+                if ($id == 0) {
+                    // print_r(get_defined_constants(true)['user']);
+                    print_r(get_included_files());
+                }
+                cli_set_process_title('-Tasker');
+            } else {
+                cli_set_process_title('-Worker');
+            }
+        } catch (\Swoole\ExitException $e) {
+            echo ($e->getStatus());
+        }
+
+    }
+
+    function task()
+    {
+
+    }
+
+    function request($request, $response)
+    {
+        $response->header('Content-Type', 'application/json');
+        if ($request->server['path_info'] == '/favicon.ico' || $request->server['request_uri'] == '/favicon.ico') {
+            $response->end();
+            return;
+        }
+
+        if (!$post = json_decode($request->getContent(), true)) {
+            $post = $request->post ?? [];
+        }
+
+        $vars = array_merge($post, $request->get ?? []);
+
+        $path = $request->server['request_uri'];
+        echo "{$path}\n";
+        //API调用
+        if ($api = $this->apiList[$path] ?? null) {
+            $res = match($api['type']) {
+                'http' => $this->http($api['url'], $vars, $request->header),
+                'rpc'  => $this->rpc($api['url'], $api['method'], [$vars], $request->header),
+            default=> [],
+            };
+            return $response->end(json_encode($res, 256));
+        }
+  		
+  		//服务发现
+        $name = strstr(substr($path, 1), '/', true);
+        if (!$srv = $this->service[$name] ?? null) {
+            return $response->end(json_encode(['code' => -1, 'message' => '服务不存在'], 256));
+        }
+
+        $res = match($srv['type']) {
+            'http' => $this->http($srv['url'] . $path, $vars, $request->header),
+            'rpc'  => $this->rpc($srv['url'], $path, [$vars], $request->header),
+        default=> [],
+        };
+        return $response->end(json_encode($res, 256));
+    }
+
+    function message()
+    {
+
+    }
+
+    private function http($url, $params, $headers = [])
+    {
+        unset(
+            $headers['set-cookie'],
+            $headers['host'],
+            $headers['content-length'],
+            $headers['user-agent'],
+            $headers['accept'],
+            $headers['accept-encoding'],
+            $headers['accept-language'],
+            $headers['connection'],
+            $headers['content-type'],
+        );
+        $res = post($url, $params, null, $headers);
+        return json_decode($res->getBody(), true) ?? [];
+    }
+
+    private function rpc($url, $method, $params, $headers = [])
+    {
+        unset(
+            $headers['set-cookie'],
+            $headers['host'],
+            $headers['content-length'],
+            $headers['user-agent'],
+            $headers['accept'],
+            $headers['accept-encoding'],
+            $headers['accept-language'],
+            $headers['connection'],
+            $headers['content-type'],
+        );
+
+        $options = [
+            "jsonrpc" => "2.0",
+            "method"  => $method,
+            "params"  => $params,
+            "id"      => time(),
+            "context" => [],
+        ];
+
+        $options = array_merge($options, $headers);
+        // print_r($headers);
+        try {
+            $res  = post($url, json_encode($options, 256))->getBody();
+            $body = json_decode($res, true);
+
+            if (isset($body['error'])) {
+                return $body['error'];
+            }
+
+        } catch (Exception $e) {
+            echo "服务连接失败\n";
+            $body['result'] = ['code' => -1, 'message' => '服务连接失败'];
+        }
+
+        return $body['result'] ?? null;
+    }
+}
