75cc50fc2857694a5300289c99afbb37fcf7f4c0
Wayren <77871399@qq.com>
2022-03-31 16:52:12 +0800

    1648716732

--- src/Helper/Console.php
+++ src/Helper/Console.php
@@ -5,7 +5,7 @@ namespace lim\Helper;
  * @Author: Wayren
  * @Date:   2022-03-29 12:12:06
  * @Last Modified by:   Wayren
- * @Last Modified time: 2022-03-31 14:01:54
+ * @Last Modified time: 2022-03-31 16:51:54
  */
 
 class Console
@@ -52,97 +52,65 @@ class Console
         shell_exec($sync);
     }
 
-    public function app()
-    {
+    // public function watcher()
+    // {
+    //     $this->scan(__LIM__, $old);
+    //     if ($this->env() == 'hyperf') {
+    //         $pid = file_get_contents(__LIM__ . '/runtime/hyperf.pid');
+    //     }
+    //     echo $pid . "\n";
+    //     for ($i = 0; $i < 10; $i++) {
+    //         sleep(10);
+    //         $this->scan(__LIM__, $new);
+    //         if ($diff = array_diff($old, $new)) {
+    //             $old = $new;
+    //             print_r($diff);
+    //             echo "重启\n";
+    //             echo shell_exec('sudo kill -15 ' . $pid);
+    //             // shell_exec("sudo php " . __LIM__ . "/bin/hyperf.php start");
+    //             $this->app('start');
+    //         }
+    //     }
+    //     // proc(function () {
+    //     //     \Swoole\Timer::tick(1000 * 5, fn() => $this->scan(__LIM__));
+    //     //     \Swoole\Event::wait();
+    //     // }, 'app-watcher');
+    // }
 
-        $action = implode(' ', $this->vars);
-        $descriptorspec = [
-                    0 => STDIN,
-                    1 => STDOUT,
-                    2 => STDERR,
-                ];
-        switch ($this->env()) {
-            case 'hyperf':
-                $script = "sudo php ".__LIM__."/bin/hyperf.php ".$action;
-                echo $script.PHP_EOL;
-                proc_open($script,$descriptorspec,$a);
-                break;
-            case 'laravel':
-                $script = "sudo php ".__LIM__."/bin/laravels ".$action;
-                echo $script.PHP_EOL;
-                proc_open($script,$descriptorspec,$a);
-                break;
-            default:
-                // code...
-                break;
-        }
-        return;
-        switch ($action) {
-            case 'watch':
-                    echo $this->env();
-                    // echo $script = "sudo php ".__LIM__."/bin/hyperf.php start";
-                    // shell_exec($script);
-                break;
+    // public function app($action=null)
+    // {
+    //     $action         ??= implode(' ', $this->vars);
+    //     $script = match ($this->env()) {
+    //         'hyperf' => "sudo php " . __LIM__ . "/bin/hyperf.php " . $action,
+    //         'laravel' => "sudo php " . __LIM__ . "/bin/laravels " . $action,
+    //         default => null,
+    //     };
+    //     $this->exec($script);
+    // }
 
-        }
+    // public function scan($dir = null, &$argv = [])
+    // {
+    //     if (is_dir($dir) && $handle = opendir($dir)) {
+    //         while (($file = readdir($handle)) !== false) {
+    //             if (($file == ".") || ($file == "..")) {
+    //                 continue;
+    //             }
+    //             $path = $dir . '/' . $file;
 
+    //             if (is_dir($path)) {
+    //                 if (in_array($file, ['.git', 'runtime', 'storage', 'vendor', 'test'])) {
+    //                     continue;
+    //                 }
+    //                 $this->scan($path, $argv);
 
-
-        // $action = array_shift($this->vars);
-
-        // switch ($action) {
-        //     case 'watch':
-        //         $this->scan(__LIM__,$old);
-        //         if ($this->env()=='hyperf') {
-        //             $pid = file_get_contents(__LIM__.'/runtime/hyperf.pid');
-        //         }
-        //         echo $pid."\n";
-        //         for ($i=0; $i <10 ; $i++) { 
-        //             sleep(10);
-        //             $this->scan(__LIM__,$new);
-        //             if ($diff=array_diff($old, $new)) {
-        //                 $old = $new;
-        //                 print_r($diff);
-        //                 echo "重启\n";
-        //                 echo shell_exec('sudo kill -10 ' . $pid);
-        //             }
-        //         }
-        //         // proc(function () {
-        //         //     \Swoole\Timer::tick(1000 * 5, fn() => $this->scan(__LIM__));
-        //         //     \Swoole\Event::wait();
-        //         // }, 'app-watcher');
-
-        //         break;
-
-        //     default:
-        //         // code...
-        //         break;
-        // }
-    }
-
-    public function scan($dir = null,&$argv=[])
-    {
-        if (is_dir($dir) && $handle = opendir($dir)) {
-            while (($file = readdir($handle)) !== false) {
-                if (($file == ".") || ($file == "..")) {
-                    continue;
-                }
-                $path = $dir . '/' . $file;
-
-                if (is_dir($path)) {
-                    if (in_array($file, ['.git', 'runtime', 'storage', 'vendor', 'test'])) {
-                        continue;
-                    }
-                    $this->scan($path,$argv);
-
-                    continue;
-                }
-                $argv[$path]=filemtime($path);
-                // echo filemtime($path)." {$path}\n";
-            }
-            closedir($handle);
-        }
-    }
+    //                 continue;
+    //             }
+    //             $argv[$path] = filemtime($path);
+    //             // echo filemtime($path)." {$path}\n";
+    //         }
+    //         closedir($handle);
+    //     }
+    // }
 
     public function fnc()
     {
@@ -211,12 +179,18 @@ class Console
         echo ' -> ' . $action . PHP_EOL;
     }
 
-    public function hf()
+    private function exec($script = null)
     {
-        // require __LIM__ . '/bin/hyperf.php';
-        print_r($this);
+        if (!$script) {
+            return;
+        }
+        $descriptorspec = [
+            0 => STDIN,
+            1 => STDOUT,
+            2 => STDERR,
+        ];
+        proc_open($script, $descriptorspec, $pipes);
     }
-
     public function __call($method, $argv)
     {
         // code...
