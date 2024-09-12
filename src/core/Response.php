11a98ccf2b9e0c4b1cf846669a82c97e057ed526
Wayren <77871399@qq.com>
2024-09-12 15:41:10 +0800

    0912

--- src/core/Response.php
+++ src/core/Response.php
@@ -36,13 +36,13 @@ class Response {
 			curr('response')->header('Access-Control-Allow-Methods', '*');
 			curr('response')->header('Access-Control-Allow-Headers', '*');
 			curr('response')->header('Content-Type', 'application/json;charset=utf-8');
-			return curr('response')->end(json_encode($result));
+			return curr('response')->end(json_encode($result, 256));
 		} else {
 			header('Access-Control-Allow-Origin:*');
 			header('Access-Control-Allow-Methods:*');
 			header('Access-Control-Allow-Headers:*');
 			header('Content-Type:application/json;charset=utf-8');
-			die(json_encode($result));
+			die(json_encode($result, 256));
 		}
 	}
 
