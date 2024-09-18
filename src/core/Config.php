07221fca0946bcb5a991ba69dcffe1dbe1ea0d46
Wayren <77871399@qq.com>
2024-09-18 13:58:07 +0800

    0918

--- src/core/Config.php
+++ src/core/Config.php
@@ -5,16 +5,18 @@ namespace lim;
 class Config {
 
 	public static function init() {
+		//加载环境变量
 		if (is_file(ROOT_PATH . '.env')) {
 			$o = parse_ini_file(ROOT_PATH . '.env');
 			foreach ($o as $k => $v) {
 				putenv($k . '=' . $v);
+				$GLOBALS['env'][$k] = $v;
 			}
 		}
-
-		ff(ROOT_PATH . 'app', function ($f) {if (str_contains($f, 'helper.php')) {include_once $f;}});
-
+		//加载配置文件
 		ff(ROOT_PATH . 'config', fn($f) => $GLOBALS['config'][preg_replace('/.+\/|.php/', '', $f)] = include $f);
+		//加载助手函数
+		ff(ROOT_PATH . 'app', function ($f) {if (str_contains($f, 'helper.php')) {include_once $f;}});
 	}
 
 	public static function get($key = '') {
--- src/functions.php
+++ src/functions.php
@@ -1,70 +1,75 @@
 <?
 declare (strict_types = 1);
+
 class limApiErr extends \Exception {}
-function array_shifter(&$o, $k = '', $v = '') {
-	$t = $o[$k] ?? $v;
-	unset($o[$k]);
-	return $t;
+
+if (!function_exists('array_shifter')) {
+	function array_shifter(&$o, $k = '', $v = '') {
+		$t = $o[$k] ?? $v;
+		unset($o[$k]);
+		return $t;
+	}
 }
 
-function token($data = '', $de = false) {
-	if ($de) {
-		if (!$ret = openssl_decrypt(base64_decode((string) $data), 'AES-128-CBC', 'service.yuwan.cn', 1, 'service.yuwan.cn')) {return null;}
-		return json_decode($ret, true);
+if (!function_exists('token')) {
+	function token($data = '', $de = false) {
+		if ($de) {
+			if (!$ret = openssl_decrypt(base64_decode((string) $data), 'AES-128-CBC', 'service.yuwan.cn', 1, 'service.yuwan.cn')) {return null;}
+			return json_decode($ret, true);
+		}
+		if (is_array($data) || is_object($data)) {$data = json_encode($data);}
+		return base64_encode(openssl_encrypt((string) $data, 'AES-128-CBC', 'service.yuwan.cn', 1, 'service.yuwan.cn'));
 	}
-	if (is_array($data) || is_object($data)) {$data = json_encode($data);}
-	return base64_encode(openssl_encrypt((string) $data, 'AES-128-CBC', 'service.yuwan.cn', 1, 'service.yuwan.cn'));
 }
 
 if (!function_exists('loger')) {
-	function loger($v = '', $type = 'debug', $file = null) {
+	function loger($v = '', $type = 'debug') {
 		if (PHP_SAPI == 'cli') {
 			$color = ['debug' => '\\e[33m', 'info' => '\\e[32m', 'err' => '\\e[31m'];
 			if (is_array($v) || is_object($v)) {$v = print_r($v, true);}
 			$str = '\\033[36m[' . date('H:i:s') . '] ' . $color[$type];
 			echo shell_exec('echo -e -n "' . $str . '"') . $v . PHP_EOL;
-		} else {
-			// echo json_encode($v);
 		}
 	}
 }
-function curr($v) {return \lim\Context::get($v);}
-function check($data = [], $rule = []) {return lim\Check::data($data)->rule($rule);}
-function http($value = '') {return lim\Http::url($value);}
-function ff($dir, $call = null) {
-	$result = [];
-	if (is_dir($dir)) {
-		$files = scandir($dir);
-		foreach ($files as $file) {
-			if ($file == '.' || $file == '..') {continue;}
-			$filePath = $dir . DIRECTORY_SEPARATOR . $file;
-			if (is_dir($filePath)) {
-				$result = array_merge($result, ff($filePath, $call));
-			} elseif (is_file($filePath)) {
-				if ($call) {$call($filePath, $result);} else { $result[] = $filePath;}
+
+if (!function_exists('ff')) {
+	function ff($dir, $call = null) {
+		$result = [];
+		if (is_dir($dir)) {
+			$files = scandir($dir);
+			foreach ($files as $file) {
+				if ($file == '.' || $file == '..') {continue;}
+				$filePath = $dir . DIRECTORY_SEPARATOR . $file;
+				if (is_dir($filePath)) {
+					$result = array_merge($result, ff($filePath, $call));
+				} elseif (is_file($filePath)) {
+					if ($call) {$call($filePath, $result);} else { $result[] = $filePath;}
+				}
 			}
 		}
-	}
-	return $result;
-}
-if (!function_exists('config')) {
-	function config($key = '') {return lim\Config::get($key);}
-}
-if (!function_exists('env')) {
-	function env($key = '', $default = '') {
-		$r = getenv($key);
-		return $r ? $r : $default;
+		return $result;
 	}
 }
-function run(callable $fn, ...$args) {
-	$s = new \Swoole\Coroutine\Scheduler();
-	$options = \Swoole\Coroutine::getOptions();
-	if (!isset($options['hook_flags'])) {
-		$s->set(['hook_flags' => SWOOLE_HOOK_ALL]);
+
+if (!function_exists('run')) {
+	function run(callable $fn, ...$args) {
+		$s = new \Swoole\Coroutine\Scheduler();
+		$options = \Swoole\Coroutine::getOptions();
+		if (!isset($options['hook_flags'])) {
+			$s->set(['hook_flags' => SWOOLE_HOOK_ALL]);
+		}
+		$s->add($fn, ...$args);
+		return $s->start();
 	}
-	$s->add($fn, ...$args);
-	return $s->start();
 }
-function redis($connection = 'default') {return \lim\Rs::connection($connection);}
-function apiErr($message = '', $code = 300) {throw new limApiErr($message, $code);}
+
+if (!function_exists('env')) {function env($key = '', $default = '') {return isset($GLOBALS['env'][$key]) ? $GLOBALS['env'][$key] : $default;}}
+if (!function_exists('config')) {function config($key = '') {return lim\Config::get($key);}}
+if (!function_exists('curr')) {function curr($v) {return \lim\Context::get($v);}}
+if (!function_exists('check')) {function check($data = [], $rule = []) {return lim\Check::data($data)->rule($rule);}}
+if (!function_exists('http')) {function http($value = '') {return lim\Http::url($value);}}
+if (!function_exists('redis')) {function redis($connection = 'default') {return \lim\Rs::connection($connection);}}
+if (!function_exists('apiErr')) {function apiErr($message = '', $code = 300) {throw new limApiErr($message, $code);}}
+
 lim\Config::init();
