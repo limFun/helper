02a3cdcad92ee2984e17465058569c8b7d878360
Wayren <77871399@qq.com>
2024-09-19 16:21:43 +0800

    0919

--- src/core/Model.php
+++ src/core/Model.php
@@ -7,7 +7,7 @@ class Model {
 	protected $table = '';
 	protected $db;
 	protected $data = [];
-	public $result = [];
+	protected $result = [];
 	function __construct($data = []) {
 		if (!$this->table) {
 			$this->table = config('db.' . $this->connection . '.prefix');
@@ -20,46 +20,87 @@ class Model {
 		$this->data = $data;
 	}
 	public static function __callStatic($method, $argv) {
-		switch (strtolower($method)) {
-		case 'h':case 'query':
-			return new static(...$argv);
-		case 'create':case 'delete':case 'update':case 'detail':
-			return self::h(...$argv)->check($method)->{$method . 'Before'}()->{$method . 'Doing'}()->{$method . 'After'}();
-		case 'list':
-			return self::h(...$argv)->{$method . 'Before'}()->{$method . 'Doing'}()->{$method . 'After'}();
-		default:
-			return call_user_func_array([self::h()->db, $method], $argv);
-		}
+		return self::query()->$method(...$argv);
 	}
-	public function __call($method, $argv) {
-		switch (strtolower($method)) {
-		case 'debug':$this->db->debug();
-			break;
-		case 'check': //数据合法性校验
-			$rule = [];
-			foreach ($this->db->schema() as $k => $v) {
-				$keyRule = $this->rule[$argv[0]][$k] ?? '';
-				$rule[$v['commit'] . '|' . $k] = $v['type'] . '|' . $keyRule;
-			}
-			check($this->data, $rule)->stop();
-			break;
-		case 'createdoing':if ($id = $this->db->insert($this->data, true)) {$this->result = Db::table($this->table)->find($id);}
-			break;
-		case 'deletedoing':$this->result['deleteRow'] = $this->db->where($this->data)->delete()->rowCount();
-			break;
-		case 'updatedoing':$this->result['updatedRow'] = $this->db->update($this->data)->rowCount();
-			break;
-		case 'listdoing':
-			$page = array_shifter($this->data, 'page', 1);
-			$limit = array_shifter($this->data, 'limit', 10);
-			$this->result['count'] = $this->db->count('*');
-			$this->result['list'] = $this->db->page($page, $limit)->select();
-			break;
-		case 'detaildoing':$this->result = $this->db->where($this->data)->find();
-			break;
-		case 'createafter':case 'deleteafter':case 'updateafter':case 'listafter':case 'detailafter': //执行后钩子
-			return $this->result;
+
+	public static function query(?array $o = []) {
+		return (new static($o))->db;
+	}
+
+	public static function h(?array $o = []) {
+		return new static($o);
+	}
+
+	public static function create(?array $o = []) {
+		return self::h($o)->check('create')->createBefore()->createDoing()->createAfter()->result();
+	}
+
+	public static function delete(?array $o = []) {
+		return self::h($o)->check('delete')->deleteBefore()->deleteDoing()->deleteAfter()->result();
+	}
+
+	public static function update(?array $o = []) {
+		return self::h($o)->check('update')->updateBefore()->updateDoing()->updateAfter()->result();
+	}
+
+	public static function detail(?array $o = []) {
+		return self::h($o)->check('detail')->detailBefore()->detailDoing()->detailAfter()->result();
+	}
+
+	public static function list(?array $o = []) {
+		return self::h($o)->listBefore()->listDoing()->listAfter()->result();
+	}
+
+	public function createDoing() {
+		if ($id = $this->db->insert($this->data, true)) {
+			$this->result = Db::table($this->table)->find($id);
 		}
+		return $this;
+	}
+
+	public function deleteDoing() {
+		$this->result['deleteRow'] = $this->db->where($this->data)->delete()->rowCount();
+		return $this;
+	}
+
+	public function updateDoing() {
+		$this->result['updatedRow'] = $this->db->update($this->data)->rowCount();
+		return $this;
+	}
+
+	public function detailDoing() {
+		$this->result = $this->db->where($this->data)->find();
+		return $this;
+	}
+
+	public function listDoing() {
+		$page = array_shifter($this->data, 'page', 1);
+		$limit = array_shifter($this->data, 'limit', 10);
+		$this->result['count'] = $this->db->count('*');
+		$this->result['list'] = $this->db->page($page, $limit)->select();
+		return $this;
+	}
+
+	public function check(string $method = '') {
+		$rule = [];
+		foreach ($this->db->schema() as $k => $v) {
+			$keyRule = $this->rule[$method][$k] ?? '';
+			$rule[$v['commit'] . '|' . $k] = $v['type'] . '|' . $keyRule;
+		}
+		check($this->data, $rule)->stop();
+		return $this;
+	}
+
+	public function debug() {
+		$this->db->debug();
+		return $this;
+	}
+
+	public function result(): mixed {
+		return $this->result;
+	}
+
+	public function __call($method, $argv) {
 		return $this;
 	}
 }
