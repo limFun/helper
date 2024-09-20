9d5a17c3f896a7ddee7f49b1c25f7f88e56ffbc5
Wayren <77871399@qq.com>
2024-09-20 15:19:24 +0800

    0920

--- src/core/Config.php
+++ src/core/Config.php
@@ -14,7 +14,11 @@ class Config {
 			}
 		}
 		//加载配置文件
-		ff(ROOT_PATH . 'config', fn($f) => $GLOBALS['config'][preg_replace('/.+\/|.php/', '', $f)] = include $f);
+		ff(ROOT_PATH . 'config', function ($f) {
+			//FPM环境不加载服务配置
+			if (PHP_SAPI !== 'cli' && str_contains($f, 'service.php')) {return;}
+			$GLOBALS['config'][preg_replace('/.+\/|.php/', '', $f)] = include_once $f;
+		});
 		//加载助手函数
 		ff(ROOT_PATH . 'app', function ($f) {if (str_contains($f, 'helper.php')) {include_once $f;}});
 	}
